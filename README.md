0b0dff17e9f9b1613fc863d68ebfc8d2b9765677
Bruno Adelé <bruno@adele.im>
2019-02-28 08:33:09 +0100

    First release 0.7.55

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docker-overpass-api
 A overpass API docker container
+
+## Build
+```
+docker build -t docker-overpass-api .
+```
+
+## Run
+```
+docker run --rm -it \
+    -e PLANET_URL="https://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.bz2" \
+    -e FLUSH_SIZE=4 \
+    -p 80:80 -v $(pwd)/data:/data docker-overpass-api
+
+# You can replace PLANET_URL by PLANET_FILE="languedoc-roussillon-latest.osm.bz2" (for use allready downloader local planet file)
+# Choose a PLANET_URL from this website => http://download.geofabrik.de/
+```
+
+## Estimation import time
+* [France](https://download.geofabrik.de/europe/france-latest.osm.bz2) => 230min(3h50)
+* [Occitanie](https://download.geofabrik.de/europe/france/languedoc-roussillon-latest.osm.bz2) => 8m30
+* [Monaco](https://download.geofabrik.de/europe/monaco-latest.osm.bz2) => 50s
+
+## Run with Overpass Turbo UI
+
+In the parameter option from http://overpass-turbo.eu/ change API server address to http://localhost/api/
---
+++ entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+data="/data"
+db="${data}/db"
+planet="${data}/planet.osm.bz2"
+imported_successful="${db}/imported_successful"
+nodemap="${db}/nodes.map"
+
+# Remove oldest socket
+socket=${db}/osm3s_v0.7.55_osm_base 
+test -e $socket && rm $socket 
+
+# If not allready imported
+if [ ! -e "$imported_successful" ]; then
+    if [ "${data}/${PLANET_FILE}" != "" ]; then
+        # Try copy local planet file
+        if [ -e "${data}/${PLANET_FILE}" ]; then
+            cp "${data}/${PLANET_FILE}" $planet
+        else
+            echo "planet file ${data}/${PLANET_FILE} not found"
+        fi
+    else
+        # Try download planet file
+        if [ ! -e "$planet" -a "${PLANET_URL}" != "" ]; then
+            echo "Downloading the ${PLANET_URL} file" 
+            wget -O $planet ${PLANET_URL}
+        fi
+    fi
+
+    # Import planet
+    if [ -e "$planet" ]; then
+        echo "Remove oldest Db database"
+        rm -rf $db
+
+        echo "Copy rules"
+        mkdir -p ${db}
+        cp -r /usr/local/src/osm-3s_v*/rules ${db}/
+
+        echo "Import planet.osm.bz2 into Db with --flush-size=${FLUSH_SIZE} option"
+        time ${OSM3_DIR}/bin/init_osm3s.sh $planet ${DB_DIR} ${OSM3_DIR} --flush-size=${FLUSH_SIZE} && touch $imported_successful
+    fi
+fi
+
+# Show imported Db status
+if [ ! -e "$imported_successful" ]; then
+    echo "[ERROR] it seem not $planet imported sucessfully "
+    
+    if [ ! -e "$nodemap" ]; then
+        echo "[ERROR] it seem not sucessfully imported $nodemap not exist"
+    fi
+else
+    ls -lh "$nodemap"
+fi
+
+# Launch CMD
+exec "$@"
